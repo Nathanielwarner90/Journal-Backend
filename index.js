c92dd127d3b71f86bb0b6c88c62f508ddea3a0eb
Nathanielwarner90 <Nathanielwarner90@gmail.com>
2025-04-29 22:56:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from journal app');
+});
+
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT, () => {
+      console.log('Routes loaded');
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error('MongoDB connection error:', err));
