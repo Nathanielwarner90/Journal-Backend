7f16abeb5a9cad6ec53463efab673c4fd8fd42dd
Nathanielwarner90 <Nathanielwarner90@gmail.com>
2025-04-30 01:41:56 -0400

    entry.js

---
+++ models/entry.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const entrySchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Entry', entrySchema);
