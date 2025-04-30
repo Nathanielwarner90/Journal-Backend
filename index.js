2f56973f588c307182f0a10f4714c73c0c4174d5
Nathanielwarner90 <Nathanielwarner90@gmail.com>
2025-04-30 01:23:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,3 +28,5 @@ mongoose.connect(process.env.MONGO_URI)
     });
   })
   .catch((err) => console.error('MongoDB connection error:', err));
+const entryRoutes = require('./routes/entries');
+app.use('/api/entries', entryRoutes);
