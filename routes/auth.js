1669000f5831e091a92e641e5d89170d3a28f95d
Nathanielwarner90 <Nathanielwarner90@gmail.com>
2025-04-29 23:15:36 -0400

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  console.log('🔥 Register route hit');
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: 'User created' });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ error: 'Registration failed' });
+  }
+});
+
+module.export = router;
